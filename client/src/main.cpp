79f3ee9ccdbe29f7cd21b35a3fa5a2b7f60f9219
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2026-04-01 21:16:19 +0200

    Basic client + echo server

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -2,17 +2,59 @@
 // Created by Leonard on 2026-03-25.
 //
 
+#include <boost/asio/awaitable.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/signal_set.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
-#include <utils.hpp>
-#include <print>
+#include <iostream>
+
+namespace asio = boost::asio;
+
+asio::awaitable<void> session(std::string host, std::string port) {
+    try {
+        asio::ip::tcp::resolver resolver(co_await asio::this_coro::executor);
+        auto endpoints =
+            co_await resolver.async_resolve(host, port, asio::use_awaitable);
+
+        asio::ip::tcp::socket socket(co_await asio::this_coro::executor);
+        co_await socket.async_connect(*endpoints.begin(), asio::use_awaitable);
+
+        while (true) {
+            std::string request;
+            std::getline(std::cin, request);
+            if (request.length() == 0) {
+                break;
+            }
+
+            co_await socket.async_write_some(
+                asio::buffer(request), asio::use_awaitable);
+
+            char response[1024];
+            size_t len = co_await socket.async_read_some(
+                asio::buffer(response), asio::use_awaitable);
+
+            std::printf("%s\n", response);
+        }
+        socket.close();
+    } catch (std::exception &e) {
+        std::printf("client exception: %s\n", e.what());
+    }
+}
 
 int main(int argc, char *argv[]) {
-    boost::beast::error_code ec;
-    std::println("{}", hello("client"));
-    for (int i = 0; i < argc; i++) {
-        std::println("{}", argv[i]);
-    }
-    return 0;
+    std::string host("127.0.0.1");
+    std::string port("3033");
+
+    asio::io_context ioc(1);
+
+    asio::signal_set signals(ioc, SIGINT, SIGTERM);
+    signals.async_wait([&ioc](auto, auto) {
+        ioc.stop();
+    });
+
+    asio::co_spawn(ioc, session(host, port), asio::detached);
+
+    ioc.run();
 }
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -2,10 +2,57 @@
 // Created by Leonard on 2026-03-25.
 //
 
-#include <utils.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/spawn.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/config.hpp>
 #include <print>
 
+namespace asio = boost::asio;
+
+asio::awaitable<void> handler(asio::ip::tcp::socket socket) {
+    try {
+        char data[1024];
+        while (true) {
+            std::size_t len = co_await socket.async_read_some(
+                asio::buffer(data), asio::use_awaitable);
+
+            std::printf("%s\n", data);
+
+            co_await async_write(
+                socket, asio::buffer(data, len), asio::use_awaitable);
+        }
+    } catch (std::exception &e) {
+        std::printf("server exception: %s\n", e.what());
+    }
+}
+
+asio::awaitable<void> listener(asio::ip::address address, unsigned short port) {
+    auto executor = co_await asio::this_coro::executor;
+    asio::ip::tcp::acceptor acceptor(executor, {address, port});
+    while (true) {
+        asio::ip::tcp::socket socket =
+            co_await acceptor.async_accept(asio::use_awaitable);
+        asio::co_spawn(executor, handler(std::move(socket)), asio::detached);
+    }
+}
+
 int main() {
-    std::println("{}", hello("server"));
+    auto address = asio::ip::make_address("127.0.0.1");
+    unsigned short port(3033);
+
+    asio::io_context ioc(1);
+
+    asio::signal_set signals(ioc, SIGINT, SIGTERM);
+    signals.async_wait([&ioc](auto, auto) {
+        ioc.stop();
+    });
+
+    asio::co_spawn(ioc, listener(address, port), asio::detached);
+
+    ioc.run();
+
     return 0;
 }
